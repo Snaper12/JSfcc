4e649c466139c421363067def1bf120972676992
Aleksey <62207853+Snaper12@users.noreply.github.com>
2022-04-11 20:31:58 +0300

    to40

---
+++ 37.js
@@ -0,0 +1,17 @@
+const myNoun = 'dog'
+const myAdjective = 'big'
+const myVerb = 'ran'
+const myAdverb = 'quickly'
+
+// Only change code below this line
+const wordBlanks =
+  'The ' +
+  myNoun +
+  ' is ' +
+  myAdjective +
+  ' and ' +
+  myVerb +
+  ' very ' +
+  myAdverb +
+  '.' // Change this line
+// Only change code above this line
---
+++ 39.js
@@ -0,0 +1,5 @@
+// Only change code below this line
+const myArray = [
+  [2, 4],
+  [6, 8],
+]
